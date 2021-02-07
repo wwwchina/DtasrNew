9b930fcd5b52a91ca6a7309b292fe36c0e77022f
wangnan <wwwsdjn@126.com>
2021-02-07 09:33:28 +0800

    init

---
+++ DtAsr/app/src/main/java/com/android/dtasr/MainActivity.java
@@ -0,0 +1,12 @@
+package com.android.dtasr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.baidu.aip.asrwakeup3.core.mini.ActivityMiniRecog;
+
+public class MainActivity extends ActivityMiniRecog {
+
+
+}
